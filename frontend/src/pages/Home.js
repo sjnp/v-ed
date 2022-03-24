907508a012928d97e93a03015873395ef798912e
sjnp <supawet.jnp@gmail.com>
2022-03-24 19:21:48 +0700

    Add experiment appbar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,17 +2,21 @@ import ResponsiveAppBar from "../components/Appbar"
 import { useSelector } from "react-redux"; // ใช้ดึงข้อมูลออกจาก Store
 import { useDispatch } from "react-redux"; // ใช้เรียก Method ที่เขียนไว้ใน Reducer
 import { setUser } from "../redux/user"; // import Method ที่เขียนไว้ใน Reducer มาเรียกใช้
+import { ThemeProvider } from "@mui/material";
 
 const Home = () => {
   const user = useSelector((state) => state.user.value)
   const dispatch = useDispatch()
-  
+
   return (
-    <section>
-      <ResponsiveAppBar/>
-      <h1>Welcome :D</h1>
-      <h3>Please Login</h3>
-    </section>
+    <ThemeProvider>
+      <section>
+        <ResponsiveAppBar />
+        <h1>Welcome :D</h1>
+        <h3>Please Login</h3>
+      </section>
+
+    </ThemeProvider>
   )
 }
 
