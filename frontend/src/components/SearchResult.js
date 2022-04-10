b82bde446b314eb3128b10912df6bd8fa82da855
MitkuSA <mit_sanbua@hotmail.com>
2022-04-10 20:24:25 +0700

    Add SearchPage and Search Component

---
+++ frontend/src/components/SearchResult.js
@@ -0,0 +1,135 @@
+// import { Container, Typography } from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import AppBarHeader from "../components/AppBarHeader";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+
+// component
+import CourseCardWide from "../components/CourseCardWide";
+
+// Material UI
+import { Stack } from "@mui/material"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+const SearchResult = () => {
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  let index = 0
+  const data = [
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    },
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      price: 500,
+      pathOnClick: '/overview'
+    }, 
+    {
+      image: `https://picsum.photos/200/300?random=${index++}`,
+      courseName: `Java programming ${index}`,
+      instructorName: `pradinan benjanavee ${index}`,
+      rating: 4.7,
+      reviewTotal: 125,
+      pathOnClick: '/overview'
+    },
+  ]
+
+  const handleData = (
+    data.map( (element, index) =>  
+      <CourseCardWide 
+        key={index}
+        image={element.image}
+        courseName={element.courseName}
+        instructorName={element.instructorName}
+        rating={element.rating}
+        reviewTotal={element.reviewTotal}
+        pathOnClick={element.pathOnClick}
+        price={element.price}
+      />    
+    )
+  )
+
+  return (
+    <Stack spacing={1.2}>
+      {handleData}
+    </Stack>
+  )
+}
+
+export default SearchResult;
--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,20 +1,25 @@
 import React from 'react'
 
 import AppBarHeader from "../components/AppBarHeader"
+import FilterSidebar from '../components/FilterSidebar'
+import SearchResult from '../components/SearchResult'
 
 // Material UI
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography';
+import {Container,Box, Grid,Typography } from '@mui/material'
 
 const Search = () => {
   return (
     <Container maxWidth="lg">
       <AppBarHeader />
-      <Typography variant="h5" sx={{ fontWeight: 'bold', marginTop: 1 }}>
-        Search
-      </Typography>
-      
-      
+     
+      <Grid container>
+        <Grid item xs={3} sx={{my : 3}}>
+          <FilterSidebar/>
+        </Grid>
+        <Grid item xs={9} sx={{my : 3}}>
+          <SearchResult/>
+        </Grid>
+    </Grid>
     </Container>
   )
 }
