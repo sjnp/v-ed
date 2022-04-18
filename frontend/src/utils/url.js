f37742a221fc63e038199eea1fd73eb0cf48cc4b
pradinanmb <pradinan.git@gmail.com>
2022-04-18 21:41:17 +0700

    merge origin sjnp and fix conflict

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -5,7 +5,7 @@ export const URL_CREATE_NEW_COURSE = '/api/instructors/course'
 export const URL_GET_CREATED_COURSE = '/api/instructors/incomplete-courses'
 export const URL_CREATE_PAR_FOR_COURSE_PIC = '/api/instructors/incomplete-courses/picture/pre-authenticated-request'
 export const URL_SAVE_COURSE_PICTURE = '/api/instructors/incomplete-courses/picture'
-
+export const URL_DELETE_COURSE_PICTURE = '/api/instructors/incomplete-courses/picture'
 
 export const URL_OVERVIEW_CATEGORY = '/api/overview/category/'
 export const URL_OVERVIEW_MY_COURSE = '/api/overview/my-course'
@@ -13,4 +13,3 @@ export const URL_OVERVIEW_COURSE_ID = '/api/overview/course/'
 export const URL_OVERVIEW_COURSE_ID_CARD = '/api/overview/course/{courseId}/card'
 
 
-export const URL_DELETE_COURSE_PICTURE = '/api/instructors/incomplete-courses/picture'
