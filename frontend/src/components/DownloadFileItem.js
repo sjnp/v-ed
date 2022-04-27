c99cd12bc4ac40a52f20c3b0aca5025cff661526
pradinanmb <pradinan.git@gmail.com>
2022-04-27 20:36:21 +0700

    Add icon download already, when downloaded

---
+++ frontend/src/components/DownloadFileItem.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { saveAs } from 'file-saver'
+
+// Material UI component
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+
+// icon
+import DownloadIcon from '@mui/icons-material/Download'
+import FileDownloadDoneIcon from '@mui/icons-material/FileDownloadDone';
+
+const DownloadFileItem = ({ url, name }) => {
+
+    const [ iconDownload, setIconDownload ] = useState(<DownloadIcon />)
+
+    const [ isDownload, setIsDownload ] = useState(false)
+
+    const handleDownload = async () => {
+        // todo : call api get preauthenticate
+        const result = await saveAs(url, name)
+        console.log(result)
+        setIconDownload(<FileDownloadDoneIcon />)
+        setIsDownload(true)
+    }
+
+    return (
+        <ListItem divider>
+            <ListItemText primary={name} />
+            <ListItemIcon>
+                <ListItemButton disabled={isDownload} onClick={handleDownload}>
+                    {iconDownload}
+                </ListItemButton>
+            </ListItemIcon>
+        </ListItem>
+    )
+}
+
+export default DownloadFileItem
--- frontend/src/components/MaterialFileDownload.js
+++ frontend/src/components/MaterialFileDownload.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { saveAs } from 'file-saver'
+
+// component
+import DownloadFileItem from './DownloadFileItem'
 
 // Material UI component
 import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
 import Divider from '@mui/material/Divider'
 import Typography from '@mui/material/Typography'
-
-// icon
-import DownloadIcon from '@mui/icons-material/Download'
+import Box from '@mui/material/Box'
 
 const MaterialFileDownload = () => {
 
@@ -28,27 +24,59 @@ const MaterialFileDownload = () => {
             url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/9vby0hrcKpe_p7-GM9TxWzEvhD2I-qvR_WOsGIE0fv3mtv_n6zjeazHVIJ-wNfRM/n/axjskktj5xlm/b/bucket-20220411-1925/o/hello.py',
             name: 'example3.py'
         },
+        //---
+        {
+            url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/p5JCMP6Mqmwu5yTUybO4LLG9cPB5R8LcB9pexMLlLEtQJXfcgF4KiA6VYNQwcCem/n/axjskktj5xlm/b/bucket-20220411-1925/o/course_pic_17.jpeg',
+            name: 'example1.jpeg'
+        },
+        {
+            url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/4zSsIDgy7IiaNcc-3CaqU5wJ6df9MPjOeDgpoxkh9rtwjUppe5qu4vXEU4VhRlZ3/n/axjskktj5xlm/b/bucket-20220411-1925/o/dummy.pdf',
+            name: 'example2.pdf'
+        },
+        {
+            url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/9vby0hrcKpe_p7-GM9TxWzEvhD2I-qvR_WOsGIE0fv3mtv_n6zjeazHVIJ-wNfRM/n/axjskktj5xlm/b/bucket-20220411-1925/o/hello.py',
+            name: 'example3.py'
+        },
+        {
+            url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/p5JCMP6Mqmwu5yTUybO4LLG9cPB5R8LcB9pexMLlLEtQJXfcgF4KiA6VYNQwcCem/n/axjskktj5xlm/b/bucket-20220411-1925/o/course_pic_17.jpeg',
+            name: 'example1.jpeg'
+        },
+        {
+            url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/4zSsIDgy7IiaNcc-3CaqU5wJ6df9MPjOeDgpoxkh9rtwjUppe5qu4vXEU4VhRlZ3/n/axjskktj5xlm/b/bucket-20220411-1925/o/dummy.pdf',
+            name: 'example2.pdf'
+        },
+        {
+            url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/9vby0hrcKpe_p7-GM9TxWzEvhD2I-qvR_WOsGIE0fv3mtv_n6zjeazHVIJ-wNfRM/n/axjskktj5xlm/b/bucket-20220411-1925/o/hello.py',
+            name: 'example3.py'
+        },
+        // {
+        //     url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/p5JCMP6Mqmwu5yTUybO4LLG9cPB5R8LcB9pexMLlLEtQJXfcgF4KiA6VYNQwcCem/n/axjskktj5xlm/b/bucket-20220411-1925/o/course_pic_17.jpeg',
+        //     name: 'example1.jpeg'
+        // },
+        // {
+        //     url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/4zSsIDgy7IiaNcc-3CaqU5wJ6df9MPjOeDgpoxkh9rtwjUppe5qu4vXEU4VhRlZ3/n/axjskktj5xlm/b/bucket-20220411-1925/o/dummy.pdf',
+        //     name: 'example2.pdf'
+        // },
+        // {
+        //     url: 'https://objectstorage.ap-singapore-1.oraclecloud.com/p/9vby0hrcKpe_p7-GM9TxWzEvhD2I-qvR_WOsGIE0fv3mtv_n6zjeazHVIJ-wNfRM/n/axjskktj5xlm/b/bucket-20220411-1925/o/hello.py',
+        //     name: 'example3.py'
+        // },
     ]
 
     return (
-        <List>
+        <Box>
             <Typography variant='subtitle1' sx={{ fontWeight: 'bold', mb: 1, mt: 1, textAlign: 'center' }}>
                 Material Files
             </Typography>
+            <List sx={{ overflow: 'auto', maxHeight: 300 }}>
             <Divider />
             {
                 files.map((file, index) => (
-                    <ListItem divider key={index}>
-                        <ListItemText primary={file.name} />
-                        <ListItemIcon>
-                            <ListItemButton onClick={() => saveAs(file.url, file.name)}>
-                                <DownloadIcon />
-                            </ListItemButton>
-                        </ListItemIcon>
-                    </ListItem>
+                    <DownloadFileItem key={index} url={file.url} name={file.name} />
                 ))
             }
-        </List>
+            </List>
+        </Box>
     )
 }
 
