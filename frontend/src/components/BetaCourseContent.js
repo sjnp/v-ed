7e3931ae6d138d7dd33d052aa6e20ff22ec85a21
pradinanmb <pradinan.git@gmail.com>
2022-05-05 05:59:09 +0700

    Add StudentContent page

---
+++ frontend/src/components/BetaCourseContent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// Material UI component
+import Box from '@mui/material/Box'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import Typography from '@mui/material/Typography'
+
+// Material UI icon
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+// component
+import ChapterVideoList from '../components/ChapterVideoList';
+
+const BetaCourseContent = ({ chapters }) => {
+
+    return (
+        <Box>
+        {
+            chapters?.map((chapter, index) => (
+                <Accordion key={index}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>{`Chapter ${index + 1}: ${chapter.name}`}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <ChapterVideoList videos={chapter.sections} />
+                    </AccordionDetails>
+                </Accordion>
+            ))
+        }
+        </Box>
+    )
+}
+
+export default BetaCourseContent
--- frontend/src/pages/StudentContent.js
+++ frontend/src/pages/StudentContent.js
@@ -1,26 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 // component
 import AppBarSearchHeader from '../components/AppBarSearchHeader'
 import StudentMenu from '../components/StudentMenu'
+import BetaCourseContent from '../components/BetaCourseContent';
 
 // Material UI component
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+// custom hook
+import useAxiosPrivate from '../hooks/useAxiosPrivate'
+
+// custom api
+import apiPrivate from '../api/apiPrivate'
+
+// url
+import { URL_GET_COURSE_BY_ID } from "../utils/url"
 
 const StudentContent = () => {
 
+    const { courseId } = useParams()
+
+    const axiosPrivate = useAxiosPrivate()
+
+    const [ content, setContent ] = useState([])
+
+    useEffect(async () => {
+
+        const url = URL_GET_COURSE_BY_ID + courseId
+        const response = await apiPrivate.get(axiosPrivate, url)
+
+        if (response.status === 200) {
+            setContent(response.data.content)
+        } else {
+            alert('Fail')
+        }
+
+    }, [])
 
     return (
         <Container>
             <AppBarSearchHeader />
             <br/>
             <Grid container>
-                <Grid item xs={3}>
+                <Grid item xs={3} md={3}>
                     <StudentMenu active='content' /> 
                 </Grid>
                 <Grid item xs={9}>
-                    Content
+
+                    <Grid item xs={12}>
+                        <Typography variant='h6'>
+                            Course content
+                        </Typography>
+                    </Grid>
+                    <Grid container>
+                        <Grid item xs={1}></Grid>
+                        <Grid item xs={10}>
+                            <br/>
+                            <BetaCourseContent chapters={content} />
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
