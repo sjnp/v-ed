094413383bcbb9111493a23251dd5ac5323a48e8
pradinanmb <pradinan.git@gmail.com>
2022-05-18 20:36:53 +0700

    Add skeleton loading home page

--- backend/src/main/java/com/ved/backend/service/CourseService.java
+++ backend/src/main/java/com/ved/backend/service/CourseService.java
@@ -18,12 +18,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
 @Service
-@Transactional
 @AllArgsConstructor
 public class CourseService {
 
--- backend/src/main/java/com/ved/backend/service/PublicService.java
+++ backend/src/main/java/com/ved/backend/service/PublicService.java
@@ -13,12 +13,10 @@ import com.ved.backend.response.OverviewResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 
 @Service
-@Transactional
 @AllArgsConstructor
 public class PublicService {
 
