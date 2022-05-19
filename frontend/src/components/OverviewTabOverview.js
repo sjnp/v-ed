e5b87374a69776da7b8d85c9a27320924ea30265
pradinanmb <pradinan.git@gmail.com>
2022-05-19 15:29:54 +0700

    Fix loading UI in overview page

--- frontend/src/components/OverviewTabOverview.js
+++ frontend/src/components/OverviewTabOverview.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// component
+import LoadingCircle from './LoadingCircle'
 
 // Materail UI component
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
 const OverviewTabOverview = ({ data }) => {
+
+    const minHeight = data ? 500 : 0
+
     return (
-        <Box minHeight={500} paddingTop={5} paddingLeft={20} paddingRight={20}>
+        <Box minHeight={minHeight} paddingTop={5} paddingLeft={20} paddingRight={20}>
+        {
+            data ?
             <Typography variant='subtitle1'>{data}</Typography>
+            :
+            <LoadingCircle loading={data ? false : true} layoutLeft={50} />
+        }    
+            
         </Box>
     )
 }
--- frontend/src/components/OverviewTabReview.js
+++ frontend/src/components/OverviewTabReview.js
@@ -8,7 +8,14 @@ const OverviewTabReview = ({ data }) => {
 
     return (
         <Box minHeight={500} paddingTop={5} paddingLeft={20} paddingRight={20}>
+        {
+            data?.length > 0 ?
             <Typography variant='subtitle1'>Add review list later</Typography>
+            :
+            <Box display='flex' flexDirection='column' textAlign='center' paddingTop={10}>
+                <Typography variant='button' color='gray'>No review now</Typography>
+            </Box>
+        }   
         </Box>
     )
 }
