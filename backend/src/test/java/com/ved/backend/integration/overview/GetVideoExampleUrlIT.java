ea8b703aefea1cc5fc0fcc424c08c8b5fe3bfaea
pradinanmb <pradinan.git@gmail.com>
2022-05-22 21:38:27 +0700

    Add @Disabled

--- backend/src/test/java/com/ved/backend/integration/overview/GetVideoExampleUrlIT.java
+++ backend/src/test/java/com/ved/backend/integration/overview/GetVideoExampleUrlIT.java
@@ -1,5 +1,6 @@
 package com.ved.backend.integration.overview;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -52,6 +53,7 @@ public class GetVideoExampleUrlIT {
 
     @Test
     @Order(2)
+    @Disabled
     public void givenCourseId_whenFound_thenReturnOkStatusAndAccessUrlVideoExample() throws Exception {
         // given
         List<Course> courses = courseRepo.findAll();
