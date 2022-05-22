6e0d4f5f357fb33b9dad91bb0d82fd2f9cc43dcb
pradinanmb <pradinan.git@gmail.com>
2022-05-22 21:29:41 +0700

    Add @Transactional

--- backend/src/test/java/com/ved/backend/util/MockDatabase.java
+++ backend/src/test/java/com/ved/backend/util/MockDatabase.java
@@ -28,8 +28,10 @@ import com.ved.backend.repo.StudentRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MockDatabase {
     
     @Autowired private AppRoleRepo appRoleRepo;
