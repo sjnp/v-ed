166d54e851f6096f06a81810aa9128dcafbf3c13
sjnp <supawet.jnp@gmail.com>
2022-06-07 21:16:46 +0700

    remove unused method from courseRepo

--- backend/src/main/java/com/ved/backend/repo/CourseRepo.java
+++ backend/src/main/java/com/ved/backend/repo/CourseRepo.java
@@ -8,43 +8,13 @@ import java.util.Optional;
 
 public interface CourseRepo extends JpaRepository<Course, Long> {
 
-  interface CourseMaterials {
-    Long getId();
-
-    String getName();
-
-    Long getPrice();
-
-    String getPictureUrl();
-
-    List<Chapter> getChapters();
-  }
-
-  interface CourseBasicInfo {
-    Long getId();
-
-    String getName();
-
-    Long getPrice();
-
-    String getPictureUrl();
-  }
-
-  CourseMaterials findCourseByInstructorAndCourseStateAndId(Instructor instructor, CourseState courseState, Long id);
-
   Optional<Course> findByInstructorAndCourseStateAndId(Instructor instructor, CourseState courseState, Long id);
-
-  Course findCourseByCourseStateAndInstructorAndId(CourseState courseState, Instructor instructor, Long id);
 
   List<Course> findCoursesByCourseState(CourseState courseState);
 
   List<Course> findCoursesByCourseStateAndInstructor(CourseState courseState, Instructor instructor);
 
   Course findCourseByCourseStateAndId(CourseState courseState, Long id);
-
-  List<CourseBasicInfo> findCoursesByInstructorAndCourseState(Instructor instructor, CourseState courseState);
-
-  Course findCourseById(Long id);
 
   // move new refactor optional below
   List<Course> findCourseByCategoryAndCourseState(Category category, CourseState courseState);
