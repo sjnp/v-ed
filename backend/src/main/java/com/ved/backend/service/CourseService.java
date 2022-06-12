97cf9168cb52081dedf8e85c97b9b939f8d806dc
pradinanmb <pradinan.git@gmail.com>
2022-06-12 16:25:08 +0700

    Clean code course service

--- backend/src/main/java/com/ved/backend/service/CourseService.java
+++ backend/src/main/java/com/ved/backend/service/CourseService.java
@@ -7,13 +7,10 @@ import com.ved.backend.model.Category;
 import com.ved.backend.model.Course;
 import com.ved.backend.model.CourseState;
 import com.ved.backend.model.StudentCourse;
-// import com.ved.backend.model.StudentCourse;
 import com.ved.backend.repo.CategoryRepo;
 import com.ved.backend.repo.CourseRepo;
 import com.ved.backend.repo.CourseStateRepo;
 import com.ved.backend.response.AboutCourseResponse;
-// import com.ved.backend.response.VideoResponse;
-// import com.ved.backend.response.CourseCardResponse;
 import com.ved.backend.response.CourseResponse;
 import com.ved.backend.response.VideoResponse;
 
@@ -33,7 +30,11 @@ public class CourseService {
     private final AuthService authService;
     private final PrivateObjectStorageService privateObjectStorageService;
 
+    private final CategoryRepo categoryRepo;
+    private final CourseStateRepo courseStateRepo;
     private final CourseRepo courseRepo;
+
+    private static final Logger log = LoggerFactory.getLogger(CourseService.class);
 
     public CourseResponse getCourseNew(Long courseId, String username) {
         StudentCourse studentCourse = authService.authorized(username, courseId);
@@ -66,52 +67,7 @@ public class CourseService {
         return privateObjectStorageService.readFile(fileName, username);
     }
 
-    ///
-    // StudentCourse studentCourse = this.authStudentCourse(username, courseId);
-    // @SuppressWarnings("unchecked")
-    // List<Map<String, String>> handouts = (List<Map<String, String>>) studentCourse.getCourse()
-    //   .getChapters()
-    //   .get(chapterIndex)
-    //   .getSections()
-    //   .get(sectionIndex)
-    //   .get("handouts");
-    // String fileName = handouts.get(handoutIndex).get("handoutUri");
-    // return privateObjectStorageService.readFile(fileName, username);
-
-    /////////////////////////////////////////////////////
-
-
-
-
-    private final CourseStateRepo courseStateRepo;
-    private final CategoryRepo categoryRepo;
-
-    // private final StudentCourseService studentCourseService;
-
-    private static final Logger log = LoggerFactory.getLogger(CourseService.class);
-
-
-
-    //////////////////////////////////////////////////////////
-
-    // public VideoResponse getVideoCourseUrl(Long courseId, int chapterIndex, int sectionIndex, String username) {
-    //     StudentCourse studentCourse = studentCourseService.auth(courseId, username);
-    //     String fileName = "course_vid_" + courseId + "_c" + chapterIndex + "_s" + sectionIndex + ".mp4";
-    //     return VideoResponse.builder()
-    //         .videoUrl(privateObjectStorageService.readFile(fileName, username))
-    //         .pictureUrl(studentCourse.getCourse().getPictureUrl())
-    //         .chapterName(studentCourse.getCourse().getChapters().get(chapterIndex).getName())
-    //         .sectionName(studentCourse.getCourse().getChapters().get(chapterIndex).getSections().get(sectionIndex).get("name").toString())
-    //         .build();
-    // }
-
-
-    // public String getVideoUrl(Long courseId, int chapterIndex, int sectionIndex, String username) {
-    //     String fileName = "course_vid_" + courseId + "_c" + chapterIndex + "_s" + sectionIndex + ".mp4";
-    //     String videoUrl = privateObjectStorageService.readFile(fileName, username);
-    // }
-
-    //////////////////////////////////////////////////////////
+    /* ****************************************************************************************** */
 
     public Course getById(Long courseId) {
         log.info("Get course by id {}", courseId);
@@ -153,12 +109,12 @@ public class CourseService {
 
 
     public Collection<CourseStateRepo.IdAndNameOnly> getAllCourseStates() {
-//        log.info("Getting all course states");
+        // log.info("Getting all course states");
         return courseStateRepo.findAllBy();
     }
 
     public Collection<CategoryRepo.IdAndNameOnly> getAllCategories() {
-//        log.info("Getting all categories");
+        // log.info("Getting all categories");
         return categoryRepo.findAllBy();
     }
 
