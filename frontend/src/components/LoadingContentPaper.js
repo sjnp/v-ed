68e94ac855300020e2c3b9e56c081d0490ce2683
pradinanmb <pradinan.git@gmail.com>
2022-06-16 23:19:45 +0700

    Fix student about course page

--- frontend/src/components/LoadingContentPaper.js
+++ frontend/src/components/LoadingContentPaper.js
@@ -1,24 +1,25 @@
 import React from 'react'
 
 // Material UI component
-import Grid from '@mui/material/Grid' 
-import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
+import Skeleton from '@mui/material/Skeleton'
 
-const ContentPaper = ({ label, content }) => {
+const LoadingContentPaper = () => {
 
     return (
         <Grid container mb={5}>
             <Grid item xs={12} mb={1} pl={1}>
-                <Typography variant='body1' fontWeight='bold' >{label}</Typography>
+                <Skeleton variant='text' width={100} />
             </Grid>
             <Grid item xs={12}>
                 <Paper sx={{ p: 3 }}>
-                    {content}
+                    <Skeleton variant='text' width='70%' />
+                    <Skeleton variant='text' width='50%' />
                 </Paper>
             </Grid>
         </Grid>
     )
 }
 
-export default ContentPaper
+export default LoadingContentPaper
--- frontend/src/pages/StudentAboutCourse.js
+++ frontend/src/pages/StudentAboutCourse.js
@@ -4,13 +4,14 @@ import { useParams } from 'react-router-dom'
 // component
 import AppBarSearchHeader from '../components/AppBarSearchHeader'
 import StudentMenu from '../components/StudentMenu'
-import LoadingCircle from '../components/LoadingCircle'
+import LoadingContentPaper from '../components/LoadingContentPaper'
 import ContentPaper from '../components/ContentPaper'
 
 // Material UI component
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import Breadcrumbs from '@mui/material/Breadcrumbs'
 
 // custom hook
 import useAxiosPrivate from '../hooks/useAxiosPrivate'
@@ -23,53 +24,59 @@ import { URL_GET_ABOUT_COURSE } from "../utils/url"
 
 const StudentAboutCourse = () => {
 
-    // const { courseId } = useParams()
+    const { courseId } = useParams()
+    const axiosPrivate = useAxiosPrivate()
 
-    // const axiosPrivate = useAxiosPrivate()
+    const [ overview, setOverview ] = useState(null)
+    const [ requirement, setRequirement ] = useState(null)
 
-    // const [ overview, setOverview ] = useState('')
-    // const [ requirement, setRequirement ] = useState('')
+    useEffect(async () => {
+        const url = URL_GET_ABOUT_COURSE.replace('{courseId}', courseId)
+        const response = await apiPrivate.get(axiosPrivate, url)
 
-    // const [ loading, setLoading ] = useState(true)
-
-    // useEffect(async () => {
-
-    //     const response = await apiPrivate.get(axiosPrivate, URL_GET_ABOUT_COURSE.replace('{courseId}', courseId))
-
-    //     if (response.status === 200) {
-    //         setOverview(response.data.overview)
-    //         setRequirement(response.data.requirement)
-    //     } else {
-    //         alert('GET about course fail')
-    //     }
-    //     setLoading(false)
-
-    // }, [])
+        if (response.status === 200) {
+            setOverview(response.data.overview)
+            setRequirement(response.data.requirement)
+        } else {
+            alert('Fail')
+        }
+    }, [])
 
     return (
         <Container>
             <AppBarSearchHeader />
-            <br/>
-            <Grid container>
+            <Grid container mt={3} mb={5}>
                 <Grid item xs={3}>
-                    <StudentMenu active='about course' /> 
+                    <StudentMenu active='about course' />
                 </Grid>
-                {/* <Grid item xs={9}>
+                <Grid item xs={9}>
                     <Grid container>
                         <Grid item xs={1}></Grid>
                         <Grid item xs={11}>
-                            <Typography variant='h6'>About course</Typography>
+                            <Breadcrumbs>
+                                <Typography color='black'>About course</Typography>
+                            </Breadcrumbs>
                         </Grid>
                     </Grid>
-                    <Grid container>
+                    <Grid container pt={3}>
                         <Grid item xs={2}></Grid>
-                        <Grid item xs={8} sx={{ pt: 4 }}>
-                            { loading ? null : <ContentPaper label='Overview' content={overview} /> }
-                            { loading ? null : <ContentPaper label='Requirement' content={requirement} /> }    
-                            <LoadingCircle loading={loading} layoutLeft={60} />
+                        <Grid item xs={8}>
+                        {
+                            overview === null ?
+                            <LoadingContentPaper />
+                            :
+                            <ContentPaper label='Overview' content={overview} />
+                        }
+                        {
+                            requirement === null ?
+                            <LoadingContentPaper />
+                            :
+                            <ContentPaper label='Requirement' content={requirement} />
+                        }
                         </Grid>
+                        <Grid item xs={2}></Grid>
                     </Grid>
-                </Grid> */}
+                </Grid>
             </Grid>
         </Container>
     )
