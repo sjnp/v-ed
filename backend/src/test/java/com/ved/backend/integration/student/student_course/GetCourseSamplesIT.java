2d90e900195c9269dfdd4cfe69d9b7de575293d7
pradinanmb <pradinan.git@gmail.com>
2022-06-17 22:46:47 +0700

    Add GetCourseSamplesIT test of student course service

---
+++ backend/src/test/java/com/ved/backend/integration/student/student_course/GetCourseSamplesIT.java
@@ -0,0 +1,115 @@
+package com.ved.backend.integration.student.student_course;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.hamcrest.Matchers.hasSize;
+
+import java.util.List;
+
+import com.ved.backend.model.Course;
+import com.ved.backend.model.Student;
+import com.ved.backend.repo.AppUserRepo;
+import com.ved.backend.repo.CourseRepo;
+import com.ved.backend.util.MockDatabase;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-it.properties")
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+public class GetCourseSamplesIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MockDatabase mockDatabase;
+
+    @Autowired
+    private AppUserRepo appUserRepo;
+
+    @Autowired
+    private CourseRepo courseRepo;
+
+    @Test
+    @Order(1)
+    public void init() throws Exception {
+        mockDatabase.clear();
+        mockDatabase.mock_app_role();
+        mockDatabase.mock_register_student();
+        mockDatabase.mock_instructor();
+        mockDatabase.mock_category();
+        mockDatabase.mock_course_state();
+        mockDatabase.mock_course(500L, "PUBLISHED", "DESIGN");
+        mockDatabase.mock_course(900L, "PUBLISHED", "PROGRAMMING");
+    }
+
+    @Test
+    @Order(2)
+    public void givenUsername_whenNoHaveData_thenReturnOkStatusAndEmptyListCourseCardResponse() throws Exception {
+        // login
+        ResultActions logiActions = mockDatabase.mock_login_student();
+        String accessToken = "Bearer " + mockDatabase.getCredential(logiActions, "access_token");
+
+        // given
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            get("/api/students/course-samples")
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
+        );
+
+        // then
+        resultActions
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    @Order(3)
+    public void givenUsername_whenHaveData_thenReturnOkStatusAndListCourseCardResponse() throws Exception {
+        // login
+        ResultActions logiActions = mockDatabase.mock_login_student();
+        String accessToken = "Bearer " + mockDatabase.getCredential(logiActions, "access_token");
+
+        // given
+        Student student = appUserRepo.findByUsername("student@test.com").getStudent();
+        List<Course> courses = courseRepo.findAll();
+        for (Course course : courses) {
+            mockDatabase.mock_student_course(student, course);
+        }
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            get("/api/students/course-samples")
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
+        );
+
+        // then
+        resultActions
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    @Order(4)
+    public void clear() {
+        mockDatabase.clear();
+    }
+    
+}
