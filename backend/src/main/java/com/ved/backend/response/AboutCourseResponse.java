8caf8eb1dcc4d07f5dd3954b1814f528ee17a95e
pradinanmb <pradinan.git@gmail.com>
2022-06-21 21:22:18 +0700

    Add attribute AboutCourseResponse

--- backend/src/main/java/com/ved/backend/response/AboutCourseResponse.java
+++ backend/src/main/java/com/ved/backend/response/AboutCourseResponse.java
@@ -17,12 +17,24 @@ public class AboutCourseResponse {
  
     private Long courseId;
 
+    private String profilePictureUrl;
+
+    private String instructorName;
+
+    private String biography;
+
+    private String occupation;
+
     private String overview;
     
     private String requirement;
 
     public AboutCourseResponse(Course course) {
         this.courseId = course.getId();
+        this.profilePictureUrl = course.getInstructor().getStudent().getProfilePicUri();
+        this.instructorName = course.getInstructor().getStudent().getFullName();
+        this.biography = course.getInstructor().getStudent().getBiography();
+        this.occupation = course.getInstructor().getStudent().getOccupation();
         this.overview = course.getOverview();
         this.requirement = course.getRequirement();
     }
