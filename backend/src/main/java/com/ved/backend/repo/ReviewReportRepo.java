35b13ff0174dc283df6ed40b5b222336ac5ff0fd
pradinanmb <pradinan.git@gmail.com>
2022-06-28 22:23:08 +0700

    Add report state & review report repo

--- backend/src/main/java/com/ved/backend/repo/ReviewReportRepo.java
+++ backend/src/main/java/com/ved/backend/repo/ReviewReportRepo.java
@@ -2,8 +2,12 @@ package com.ved.backend.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ved.backend.model.Review;
 import com.ved.backend.model.ReviewReport;
+import com.ved.backend.model.Student;
 
 public interface ReviewReportRepo extends JpaRepository<ReviewReport, Long> {
     
+    Boolean existsByReviewAndStudent(Review review, Student student);
+
 }
