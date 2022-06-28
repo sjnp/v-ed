53c0e3cbb051a70968c2d4ca239b73ec3544300f
pradinanmb <pradinan.git@gmail.com>
2022-06-28 22:38:38 +0700

    Add report post service

--- backend/src/main/java/com/ved/backend/repo/PostReportRepo.java
+++ backend/src/main/java/com/ved/backend/repo/PostReportRepo.java
@@ -2,8 +2,12 @@ package com.ved.backend.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ved.backend.model.Post;
 import com.ved.backend.model.PostReport;
+import com.ved.backend.model.Student;
 
 public interface PostReportRepo extends JpaRepository<PostReport, Long> {
  
+    Boolean existsByPostAndStudent(Post post, Student student);
+
 }
--- backend/src/main/java/com/ved/backend/service/ReportService.java
+++ backend/src/main/java/com/ved/backend/service/ReportService.java
@@ -9,15 +9,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.ved.backend.configuration.ReportStateProperties;
+import com.ved.backend.exception.PostNotFoundException;
 import com.ved.backend.exception.ReasonReportNotFoundException;
 import com.ved.backend.exception.ReviewNotFoundException;
 import com.ved.backend.exception.baseException.BadRequestException;
 import com.ved.backend.exception.baseException.ConflictException;
+import com.ved.backend.model.Post;
+import com.ved.backend.model.PostReport;
 import com.ved.backend.model.ReasonReport;
 import com.ved.backend.model.ReportState;
 import com.ved.backend.model.Review;
 import com.ved.backend.model.ReviewReport;
 import com.ved.backend.model.Student;
+import com.ved.backend.repo.PostRepo;
+import com.ved.backend.repo.PostReportRepo;
 import com.ved.backend.repo.ReasonReportRepo;
 import com.ved.backend.repo.ReportStateRepo;
 import com.ved.backend.repo.ReviewRepo;
@@ -38,6 +43,9 @@ public class ReportService {
     private final ReviewRepo reviewRepo;
     private final ReviewReportRepo reviewReportRepo;
     private final ReportStateRepo reportStateRepo;
+
+    private final PostRepo postRepo;
+    private final PostReportRepo postReportRepo;
 
     private final ReportStateProperties reportStateProperties;
 
@@ -87,7 +95,7 @@ public class ReportService {
             .orElseThrow(() -> new ReviewNotFoundException(reportRequest.getContentId()));
         
         if (reviewReportRepo.existsByReviewAndStudent(review, student)) {
-            throw new ConflictException("You report this review alreay");
+            throw new ConflictException("You report this review already");
         }
 
         ReportState reportState = reportStateRepo.findByName(reportStateProperties.getPending());
@@ -104,7 +112,25 @@ public class ReportService {
     }
 
     public void reportPost(String username, ReportRequest reportRequest) {
+        Student student = userService.getStudent(username);
+        Post post = postRepo.findById(reportRequest.getContentId())
+            .orElseThrow(() -> new PostNotFoundException(reportRequest.getContentId()));
 
+        if (postReportRepo.existsByPostAndStudent(post, student)) {
+            throw new ConflictException("You report this post already");
+        }
+
+        ReportState reportState = reportStateRepo.findByName(reportStateProperties.getPending());
+        ReasonReport reasonReport = reasonReportRepo.findById(reportRequest.getReasonReportId())
+            .orElseThrow(() -> new ReasonReportNotFoundException(reportRequest.getReasonReportId()));
+        PostReport postReport = PostReport.builder()
+            .reasonReport(reasonReport)
+            .reportState(reportState)
+            .post(post)
+            .student(student)
+            .datatime(LocalDateTime.now())
+            .build();
+        postReportRepo.save(postReport);
     }
 
     public void reportComment(String username, ReportRequest reportRequest) {
