c4907614932c9dc205c62b53ef6f62bbf015e56a
pradinanmb <pradinan.git@gmail.com>
2022-07-05 19:57:36 +0700

    Add FilterCategory component

---
+++ frontend/src/components/FilterCategory.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+// Material UI component
+import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+
+// Material UI icon
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+// feature
+import { setCategory } from '../features/searchSlice'
+
+const FilterCategory = () => {
+
+    const dispatch = useDispatch()
+
+    const categories = ['All', 'Academic', 'Art', 'Business', 'Design', 'Programming']
+    
+    const [ active, setActive ] = useState('All')
+
+    const handleClickCategory = (value) => {
+        setActive(value)
+        dispatch( setCategory({ category: value }) )
+    }
+
+    return (
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography fontWeight='bold'>Category</Typography>
+                <Typography color='gray' pl={1}>{`( ${active} )`}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <List>
+                {
+                    categories.map((category, index) => (
+                        <ListItemButton key={index} selected={active === category} onClick={() => handleClickCategory(category)}>
+                            <ListItemText primary={category} />
+                        </ListItemButton>
+                    ))
+                }
+                </List>
+            </AccordionDetails>
+        </Accordion>
+    )
+}
+
+export default FilterCategory
