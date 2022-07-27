a8dd1582dc16b64156c371f60a50f978ddc11bed
pradinanmb <pradinan.git@gmail.com>
2022-07-27 21:12:13 +0700

    Add update display it test

---
+++ backend/src/test/java/com/ved/backend/integration/user/UpdateDisplayIT.java
@@ -0,0 +1,70 @@
+package com.ved.backend.integration.user;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.ved.backend.util.MockDatabase;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-it.properties")
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+public class UpdateDisplayIT {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MockDatabase mockDatabase;
+
+    @Test
+    @Order(1)
+    public void init() throws Exception {
+        mockDatabase.clear();
+        mockDatabase.mock_app_role();
+        mockDatabase.mock_register_student();
+    }
+
+    @Test
+    @Order(2)
+    public void givenUsername_whenSuccess_thenReturnOkStatusAndNewDisplayUrl() throws Exception {
+        // login
+        ResultActions logiActions = mockDatabase.mock_login_student();
+        String accessToken = "Bearer " + mockDatabase.getCredential(logiActions, "access_token");
+
+        // given
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+            put("/api/users/display")
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
+        );
+
+        // then
+        resultActions
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$").isNotEmpty())
+            .andExpect(jsonPath("$").isString());
+    }
+
+    @Test
+    @Order(3)
+    public void clear() throws Exception {
+        mockDatabase.clear();
+    }
+
+}
